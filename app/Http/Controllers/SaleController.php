e6398657f3ab2df1446d4fbddead343be4c82c9c
hilit van pelt <hilit41@gmail.com>
2020-07-08 17:45:30 +0300

    remove commants

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -34,19 +34,19 @@ class SaleController extends Controller
        $price = $request->price;
       // $responseFroPayMe = $this.insertSale($description,$currency,$price);
         $responseFroPayMe = self::insertSale($description,$currency,$price);
-//        $sale = new Sale;
-//        $sale->description = $description;
-//        $sale->currency=$responseFroPayMe->currency;
-//        $sale->price=$responseFroPayMe->price;
-//        $sale->sale_url=$responseFroPayMe->sale_url;
-//        $sale->status_code=$responseFroPayMe->status_code;
-//        $sale->payme_sale_code=$responseFroPayMe->payme_sale_code;
-//        $sale->payme_sale_id=$responseFroPayMe->payme_sale_id;
-//        $sale->price=$responseFroPayMe->price;
-//        $sale->transaction_id=$responseFroPayMe->transaction_id;
-//        $sale->status_code = $responseFroPayMe->status_code;
-//
-//        $sale->save();
+        $sale = new Sale;
+        $sale->description = $description;
+        $sale->currency=$responseFroPayMe->currency;
+        $sale->price=$responseFroPayMe->price;
+        $sale->sale_url=$responseFroPayMe->sale_url;
+        $sale->status_code=$responseFroPayMe->status_code;
+        $sale->payme_sale_code=$responseFroPayMe->payme_sale_code;
+        $sale->payme_sale_id=$responseFroPayMe->payme_sale_id;
+        $sale->price=$responseFroPayMe->price;
+        $sale->transaction_id=$responseFroPayMe->transaction_id;
+        $sale->status_code = $responseFroPayMe->status_code;
+
+        $sale->save();
        return redirect('/sales');
 
 
@@ -55,9 +55,8 @@ class SaleController extends Controller
 
     public function insertSale($description,$currency,$price)
     {
-
-        //
         $client = new Client();
+        $res =null;
        // $client = new Client(['verify' => '/my/path/to/mycertfile.pem']);
         try {
             $res = $client->request('POST', 'https://preprod.paymeservice.com/api/generate-sale', [
@@ -81,6 +80,7 @@ class SaleController extends Controller
         // 'application/json; charset=utf8'
         echo $res->getBody();
         // {"type":"User"...'
-
+        return $res;
     }
+
 }
